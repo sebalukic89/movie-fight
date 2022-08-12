215b4b289ba00dd92fd62edb465b297940176459
Sebastian Lukich <sebastianlukich@Sebastians-MacBook-Pro.local>
2022-08-12 14:31:27 -0400

    make HTTP request secure

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autocompleteConfig = {
 	},
 
 	async fetchData(searchTerm) {
-		const response = await axios.get('http://www.omdbapi.com/', {
+		const response = await axios.get('https://www.omdbapi.com/', {
 			params: {
 				apikey: '36109d03',
 				s: searchTerm,
